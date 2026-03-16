f70dcf391f773b94788ad5e41675dcc6921171b2
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-17 00:29:54 +0100

    feat: bootstrap MapCanvas.tsx with basic structure

---
+++ src/components/editors/map-editor/MapCanvas.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import { GB_COLORS } from "../../../constants/colors";
+import type {
+import type { Chunk } from "../../../types/map";
+import { CHUNK_SIZE } from "../../../types/map";
+import {
+import type { CellCoords } from "./types";
