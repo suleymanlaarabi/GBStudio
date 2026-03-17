41d302a0832ca193c3a033543b16b998a520a6b5
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-17 03:51:54 +0100

    feat: bootstrap AnimationList.tsx with basic structure

---
+++ src/components/editors/sprite-studio/AnimationList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Plus, X } from "lucide-react";
+import { useStore } from "../../../store";
+import type { SpriteAnimation, Tileset } from "../../../types";
+import { AnimationPreview } from "./AnimationPreview";
