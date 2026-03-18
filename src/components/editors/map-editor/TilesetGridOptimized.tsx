6b10d2e07f1bf66751a29ed308c9085e74fcfb58
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-18 11:11:54 +0100

    feat: bootstrap TilesetGridOptimized.tsx with basic structure

---
+++ src/components/editors/map-editor/TilesetGridOptimized.tsx
@@ -0,0 +1,3 @@
+import React, { useRef, useEffect, useMemo, useState, useCallback } from "react";
+import { GB_COLORS } from "../../../constants/colors";
+import type { Tileset } from "../../../types";
