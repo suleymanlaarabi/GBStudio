70132cd564eafbf334352d8dfbded3584a088177
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-18 03:36:54 +0100

    feat: bootstrap SpriteAssetLibrary.tsx with basic structure

---
+++ src/components/editors/sprite-studio/SpriteAssetLibrary.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Database } from "lucide-react";
+import { GB_COLORS } from "../../../constants/colors";
+import type { Tileset, Tile } from "../../../types";
+import { CustomSelect } from "../../ui/CustomSelect";
