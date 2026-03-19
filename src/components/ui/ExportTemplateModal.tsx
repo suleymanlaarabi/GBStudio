1e9138eaf1ada5064728874f3154e0c71a489a10
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-19 22:00:54 +0100

    feat: bootstrap ExportTemplateModal.tsx with basic structure

---
+++ src/components/ui/ExportTemplateModal.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { X, Download, Package } from "lucide-react";
+import {
+import type { TileMap, Tileset, SpriteAsset, SoundAsset } from "../../types";
+import type { TemplateCategory } from "../../types/template";
