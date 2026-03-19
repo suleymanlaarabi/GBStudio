a88f54b67538d60bf2dd1776b62975d176aa954e
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-19 19:10:54 +0100

    feat: implement core logic for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+  plugins: [react()],
+
+  clearScreen: false,
+
+  build: {
+    target: "esnext",
+    minify: "terser",
+    terserOptions: {
+      compress: {
