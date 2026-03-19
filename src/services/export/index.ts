497e1407967929917189b8180eb72bc8e497efdb
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-19 09:23:54 +0100

    feat: finalize index.ts implementation

--- src/services/export/index.ts
+++ src/services/export/index.ts
@@ -4,3 +4,28 @@ import { allocateRomBanks } from "./romAllocator";
 import { buildMapExport } from "./mapBuilder";
 import { buildHeader } from "./headerGen";
 import { buildSource } from "./sourceGen";
+
+export const generateCFile = (
+  projectName: string,
+  tilesets: Tileset[],
+  maps: TileMap[],
+  sprites: SpriteAsset[],
+  sounds: SoundAsset[] = [],
+): string => {
+  const expandedTilesets = tilesets.map(expandTileset);
+  const mapExports = maps.map((map) => buildMapExport(map, tilesets));
+  const romPlan = allocateRomBanks(mapExports);
+  return buildSource(projectName, expandedTilesets, mapExports, sprites, romPlan, sounds);
+};
+
+export const generateHFile = (
+  projectName: string,
+  tilesets: Tileset[],
+  maps: TileMap[],
+  sprites: SpriteAsset[],
+  sounds: SoundAsset[] = [],
+): string => {
+  const mapExports = maps.map((map) => buildMapExport(map, tilesets));
+  const romPlan = allocateRomBanks(mapExports);
+  return buildHeader(projectName, mapExports, sprites, romPlan, sounds);
+};
