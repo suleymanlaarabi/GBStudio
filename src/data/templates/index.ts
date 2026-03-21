559a2afe2db0f7db94caf67bc78c4edc9cbf24ed
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-21 05:10:54 +0100

    feat: finalize index.ts implementation

--- src/data/templates/index.ts
+++ src/data/templates/index.ts
@@ -8,3 +8,15 @@ import { VILLAGE_TEMPLATE } from "./village";
 import { SOUNDS_TEMPLATE } from "./sounds";
 import type { Template } from "../../types/template";
 import { SUPER_MARIO_LAND_TEMPLATE } from "./mario";
+
+export const BUILTIN_TEMPLATES: Template[] = [
+  DUNGEON_TEMPLATE,
+  VILLAGE_TEMPLATE,
+  PLATFORMER_TEMPLATE,
+  PLATFORMER_PRO_TEMPLATE,
+  SUPER_MARIO_LAND_TEMPLATE,
+  SOUNDS_TEMPLATE,
+  SPACE_TEMPLATE,
+  SPRITE_PACK_TEMPLATE,
+  OVERWORLD_TEMPLATE,
+];
