40cda3aae2d7afa4bda609e8588cac84d88ff4dc
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-22 07:57:54 +0100

    feat: implement core logic for selectionSlice.ts

--- src/store/slices/selectionSlice.ts
+++ src/store/slices/selectionSlice.ts
@@ -3,3 +3,46 @@ import type { GBColor, SelectionBounds, SelectionState, Tileset } from "../../ty
 import { clearTileArea, fillTileArea } from "../../services/tileService";
 import {
 import { normalizeSelection, selectAll } from "../../utils";
+  copySelectionContent,
+  cutSelectionContent,
+  deleteSelectionContent,
+  moveSelectionContent,
+  pasteSelectionContent,
+  flipSelectionHorizontal as flipServiceSelectionHorizontal,
+  flipSelectionVertical as flipServiceSelectionVertical,
+  rotateSelectionClockwise as rotateServiceClockwise,
+  rotateSelectionCounterClockwise as rotateServiceCounterClockwise,
+} from "../../services/selectionService";
+
+export interface SelectionSlice {
+  selection: SelectionState;
+  clipboard: (GBColor | null)[][] | null;
+  beginSelection: (x: number, y: number) => void;
+  updateSelection: (x: number, y: number) => void;
+  endSelection: () => void;
+  clearSelection: () => void;
+  selectAll: () => void;
+  copySelection: () => void;
+  cutSelection: () => void;
+  pasteSelection: (x: number, y: number) => void;
+  deleteSelection: () => void;
+  moveSelection: (deltaX: number, deltaY: number) => void;
+  updateSelectionBounds: (x: number, y: number) => void;
+  flipSelectionHorizontal: () => void;
+  flipSelectionVertical: () => void;
+  rotateSelectionClockwise: () => void;
+  rotateSelectionCounterClockwise: () => void;
+  fillArea: (
+    tilesetIndex: number,
+    tileIndex: number,
+    selection: SelectionBounds,
+    color: GBColor,
+  ) => void;
+  clearArea: (tilesetIndex: number, tileIndex: number, selection: SelectionBounds) => void;
+}
+
+type SelectionStoreState = SelectionSlice & {
+  activeTileIndex: number;
+  activeTilesetIndex: number;
+  commit: () => void;
+  tilesets: Tileset[];
