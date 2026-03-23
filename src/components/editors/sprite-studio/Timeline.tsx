1accaa6b84b21bd00443c21bf4a19ea95586210c
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-23 07:03:54 +0100

    feat: bootstrap Timeline.tsx with basic structure

---
+++ src/components/editors/sprite-studio/Timeline.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Clock, Plus, X } from "lucide-react";
+import { GB_COLORS } from "../../../constants/colors";
+import type { SpriteAnimation, Tile, Tileset } from "../../../types";
