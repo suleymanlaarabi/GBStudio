7d0f3c1b0639c7c3cdf79c69f9a208e2cbf5cae1
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-24 02:01:54 +0100

    feat: finalize fill.ts implementation

--- src/utils/drawing/fill.ts
+++ src/utils/drawing/fill.ts
@@ -21,3 +21,25 @@ export function floodFillData(
     const [x, y] = stack.pop()!;
     if (x < 0 || x >= tileSize || y < 0 || y >= tileSize) continue;
     if ((newData[y]![x] ?? null) !== startColor) continue;
+
+    newData[y]![x] = targetColor;
+    stack.push([x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]);
+  }
+
+  return newData;
+}
+
+export const fillArea = (
+  data: PixelData,
+  selection: SelectionBounds,
+  color: GBColor,
+) =>
+  drawRectangle(
+    data,
+    selection.x,
+    selection.y,
+    selection.width,
+    selection.height,
+    color,
+    true,
+  );
