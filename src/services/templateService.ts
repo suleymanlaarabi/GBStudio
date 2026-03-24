551f25ed4f2e6f678a0ce6b3dcc1607670a1f409
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-24 14:51:54 +0100

    feat: bootstrap templateService.ts with basic structure

---
+++ src/services/templateService.ts
@@ -0,0 +1,3 @@
+import { BUILTIN_TEMPLATES } from "../data/templates";
+import type { Template, TemplateCategory, TemplateFile } from "../types/template";
+import type { TileMap, Tileset, SpriteAsset, SoundAsset } from "../types";
