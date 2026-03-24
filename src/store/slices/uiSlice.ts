61f55344f9233d9cfae4a2d1fef43326f58bfde5
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-24 05:06:54 +0100

    feat: implement core logic for uiSlice.ts

--- src/store/slices/uiSlice.ts
+++ src/store/slices/uiSlice.ts
@@ -1,2 +1,17 @@
 import type { StateCreator } from "zustand";
 import type { GBColor, MapTool, Tool, View } from "../../types";
+
+export interface UISlice {
+  activeTilesetIndex: number;
+  activeTileIndex: number;
+  activeMapIndex: number;
+  activeSpriteIndex: number;
+  activeLayerIndex: number;
+  selectedColor: GBColor;
+  tool: Tool;
+  mapTool: MapTool;
+  mapShapeFilled: boolean;
+  view: View;
+  zoom: number;
+  selectedSpriteInstanceId: string | null;
+  activeLayerIsWindow: boolean;
