b7a82d242222c57839408a21b2364960bad52f74
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-25 14:18:54 +0100

    feat: finalize vite.config.ts implementation

--- vite.config.ts
+++ vite.config.ts
@@ -30,3 +30,19 @@ export default defineConfig(async () => ({
   },
 
   server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
