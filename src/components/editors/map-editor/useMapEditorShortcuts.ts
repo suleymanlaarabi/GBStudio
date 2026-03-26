1906f111f715b503857a2ff1859f419ee5a0bbcc
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-26 09:23:54 +0100

    feat: implement core logic for useMapEditorShortcuts.ts

--- src/components/editors/map-editor/useMapEditorShortcuts.ts
+++ src/components/editors/map-editor/useMapEditorShortcuts.ts
@@ -18,3 +18,20 @@ export const useMapEditorShortcuts = ({ hoverCell }: UseMapEditorShortcutsProps)
   } = useStore();
 
   useKeyboardShortcuts({
+    shortcuts: [
+      {
+        matcher: (event) =>
+          (event.ctrlKey || event.metaKey) && event.key.toLowerCase() === "c",
+        handler: (event) => {
+          event.preventDefault();
+          copyMapSelection();
+        },
+      },
+      {
+        matcher: (event) =>
+          (event.ctrlKey || event.metaKey) && event.key.toLowerCase() === "x",
+        handler: (event) => {
+          event.preventDefault();
+          cutMapSelection();
+        },
+      },
