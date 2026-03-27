7af4a4dae0de2ba7b0e1308edd737ac4435ba313
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-27 09:10:54 +0100

    feat: implement core logic for AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,3 +2,20 @@ import React from "react";
 import { Sidebar } from "./Sidebar";
 import { StatusBar, type StatusTone } from "./StatusBar";
 import { TitleBar } from "./TitleBar";
+
+interface AppLayoutProps {
+  children: React.ReactNode;
+  isSaving: boolean;
+  onExportToProject: () => void;
+  onDownload: () => void;
+  onChooseProjectFolder: () => void;
+  onOpenProject: () => void;
+  onSaveProject: () => void;
+  onSaveProjectAs: () => void;
+  projectPath: string | null;
+  projectFilePath: string | null;
+  statusMessage: string;
+  statusTone: StatusTone;
+  onOpenShortcuts: () => void;
+  onOpenTemplates: () => void;
+}
