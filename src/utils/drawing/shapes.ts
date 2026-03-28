3b8f14be3337b23124351536be986c52256c2953
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-28 06:46:54 +0100

    feat: implement core logic for shapes.ts

--- src/utils/drawing/shapes.ts
+++ src/utils/drawing/shapes.ts
@@ -1,1 +1,16 @@
 import type { GBColor } from "../../types";
+
+type PixelData = (GBColor | null)[][];
+
+export function getCircleFromBounds(
+  minX: number,
+  minY: number,
+  maxX: number,
+  maxY: number,
+): { centerX: number; centerY: number; radiusX: number; radiusY: number } {
+  return {
+    centerX: (minX + maxX) / 2,
+    centerY: (minY + maxY) / 2,
+    radiusX: (maxX - minX) / 2,
+    radiusY: (maxY - minY) / 2,
+  };
