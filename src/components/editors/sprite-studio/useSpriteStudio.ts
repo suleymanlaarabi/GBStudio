83d15a264cd2f7aea2cea0c5b1a01430b6f2a5cc
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-29 13:01:54 +0200

    feat: implement core logic for useSpriteStudio.ts

--- src/components/editors/sprite-studio/useSpriteStudio.ts
+++ src/components/editors/sprite-studio/useSpriteStudio.ts
@@ -1,2 +1,21 @@
 import { useState } from "react";
 import { useStore } from "../../../store";
+
+export const useSpriteStudio = () => {
+  const {
+    sprites,
+    tilesets,
+    activeTilesetIndex,
+    setActiveTileset,
+    addSprite,
+    addAnimation,
+    addFrame,
+    updateFrameDuration,
+    removeFrame,
+    removeAnimation,
+    removeSprite,
+    activeTileIndex,
+    setActiveTile,
+    selectedSpriteId,
+    setSelectedSpriteId,
+    selectedAnimId,
