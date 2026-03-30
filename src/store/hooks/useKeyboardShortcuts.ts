4767ea043fbb1993a3ef2374622dae3367a4f8d4
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-30 13:57:54 +0200

    feat: implement core logic for useKeyboardShortcuts.ts

--- src/store/hooks/useKeyboardShortcuts.ts
+++ src/store/hooks/useKeyboardShortcuts.ts
@@ -1,2 +1,20 @@
 import { useEffect } from "react";
 import { isEditableElement } from "../../utils/keyboard";
+
+type ShortcutHandler = (event: KeyboardEvent) => void;
+
+interface ShortcutDefinition {
+  handler: ShortcutHandler;
+  matcher: (event: KeyboardEvent) => boolean;
+  allowInEditable?: boolean;
+}
+
+interface UseKeyboardShortcutsOptions {
+  enabled?: boolean;
+  shortcuts: ShortcutDefinition[];
+}
+
+export const useKeyboardShortcuts = ({
+  enabled = true,
+  shortcuts,
+}: UseKeyboardShortcutsOptions) => {
