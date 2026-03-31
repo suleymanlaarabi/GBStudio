ee0c338cec9bf367c38aa5527701f65060daf00d
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-31 21:48:54 +0200

    feat: bootstrap SoundEditorPanel.tsx with basic structure

---
+++ src/components/editors/sound-studio/SoundEditorPanel.tsx
@@ -0,0 +1,5 @@
+import React, { useRef } from "react";
+import { Play } from "lucide-react";
+import type {
+import { CustomSelect } from "../../ui/CustomSelect";
+import { playSoundPreview } from "./soundPreview";
