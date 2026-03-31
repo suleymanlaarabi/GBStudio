574658d4a14f0754f37ed99dacc18cbcfab4cef7
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-31 20:04:54 +0200

    feat: implement core logic for selectionService.ts

--- src/services/selectionService.ts
+++ src/services/selectionService.ts
@@ -1,3 +1,21 @@
 import type { GBColor, SelectionBounds } from "../types";
 import {
 import {
+  applySelectionContent,
+  clearArea,
+  extractSelection,
+  moveArea,
+} from "../utils";
+  flipTileDataHorizontal as flipHorizontal,
+  flipTileDataVertical as flipVertical,
+  rotateTileDataClockwise,
+  rotateTileDataCounterClockwise,
+} from "../services/tileService";
+
+type PixelData = (GBColor | null)[][];
+
+export const copySelectionContent = (
+  data: PixelData,
+  selection: SelectionBounds,
+) => extractSelection(data, selection);
+
