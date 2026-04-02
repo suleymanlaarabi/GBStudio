a3cbc9ee0549e2d58840aafc754e8e4fcd87432a
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-04-02 17:49:54 +0200

    feat: implement core logic for sounds.ts

--- src/data/templates/sounds.ts
+++ src/data/templates/sounds.ts
@@ -100,3 +100,28 @@ const SOUNDS: Omit<SoundAsset, "id">[] = [
     name: "Confirm",
     type: "PULSE1",
     pulse1: {
+      sweepTime: 0,
+      sweepDirection: "UP",
+      sweepShift: 0,
+      duty: 2,
+      length: 6,
+      initialVolume: 11,
+      envelopeDirection: "DOWN",
+      envelopeSweep: 3,
+      frequency: 1862,
+    },
+  },
+  // ── WARP / TELEPORT ───────────────────────────────────────────────────────
+  {
+    name: "Warp",
+    type: "WAVE",
+    wave: {
+      dacEnabled: true,
+      length: 30,
+      volumeCode: 2,
+      frequency: 1750,
+      waveData: [
+        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5,
+        6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+      ],
+    },
