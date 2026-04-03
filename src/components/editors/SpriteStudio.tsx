31b82e231a1586114bbf96ef4c9309f5052132d2
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-04-03 03:30:54 +0200

    feat: bootstrap SpriteStudio.tsx with basic structure

---
+++ src/components/editors/SpriteStudio.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Modal } from "../ui/Modal";
+import { useSpriteStudio } from "./sprite-studio/useSpriteStudio";
+import { SpriteSidebar } from "./sprite-studio/SpriteSidebar";
+import { AnimationList } from "./sprite-studio/AnimationList";
+import { Timeline } from "./sprite-studio/Timeline";
+import { SpriteAssetLibrary } from "./sprite-studio/SpriteAssetLibrary";
