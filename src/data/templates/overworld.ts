8c1eb58d6f7304601d33297c7f791b705f6f1a3b
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-04-06 12:31:54 +0200

    feat: implement core logic for overworld.ts

--- src/data/templates/overworld.ts
+++ src/data/templates/overworld.ts
@@ -67,3 +67,36 @@ const TILES = {
     [3, 2, 0, 2, 2, 0, 2, 3],
     [3, 3, 2, 2, 2, 2, 3, 3],
   ]) },
+  ship1: { id: "bo-c0", size: 8 as const, data: d([
+    [null, null, 1, 1, null, null, null, null],
+    [null, 1, 1, 1, 1, null, null, null],
+    [1, 1, 1, 1, 1, 1, null, null],
+    [null, 3, 3, 3, 3, 3, 3, null],
+    [3, 2, 2, 2, 2, 2, 2, 3],
+    [3, 2, 2, 2, 2, 2, 2, 3],
+    [null, 3, 3, 3, 3, 3, 3, null],
+    [null, null, null, null, null, null, null, null],
+  ]) },
+  ship2: { id: "bo-c1", size: 8 as const, data: d([
+    [null, null, 1, 1, null, null, null, null],
+    [null, 1, 1, 1, 1, null, null, null],
+    [1, 1, 1, 1, 1, 1, null, null],
+    [null, 3, 3, 3, 3, 3, 3, null],
+    [3, 2, 2, 2, 2, 2, 2, 3],
+    [3, 2, 2, 2, 2, 2, 2, 3],
+    [null, 3, 3, 3, 3, 3, 3, null],
+    [null, null, 0, 0, 0, 0, null, null],
+  ]) },
+};
+
+const G = { tilesetId: TS, tileIndex: 0 };
+const W = { tilesetId: TS, tileIndex: 1 };
+const Tr = { tilesetId: TS, tileIndex: 2 };
+const P = { tilesetId: TS, tileIndex: 3 };
+const M = { tilesetId: TS, tileIndex: 4 };
+const H = { tilesetId: TS, tileIndex: 5 };
+const n = null;
+
+const CTS = "builtin-overworld-chars";
+
+export const OVERWORLD_TEMPLATE: Template = {
